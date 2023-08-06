e66880ed6b098d2623a5f074456a0e7c25d87e47
Serg <seleznevs79@mail.ru>
2023-08-06 12:43:50 +0700

    hw4 z3

--- Program.cs
+++ Program.cs
@@ -38,11 +38,12 @@
 // }
 // Console.WriteLine($"Сумма числа {num} = {sum}");
 
+
+
 // С Методом
 
 //  Console.Write("Введите число: ");
 //   int num = int.Parse(Console.ReadLine()!);
-
 //   Console.WriteLine($"Сумма числа {num} = {GetSum(num)}");
 
 // // // Методы
@@ -57,7 +58,22 @@
 
 
 
-// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. (можно указать любой промежуток)
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// (можно указать любой промежуток)
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
+
+
+int[] array = GetRange(8);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+
+//method
+int[] GetRange(int size){
+int[] massive = new int[size];
+for(int i = 0; i < massive.Length; i++){
+massive[i] = new Random().Next(0,20);
+}
+return massive;
+}
